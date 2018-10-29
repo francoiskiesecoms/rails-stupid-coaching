cc45d4f2a8306dda65dfc6ba7768e3f795b83cb2
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2018-10-29 11:41:15 +0100

    basics done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'questions/ask'
+  get 'ask', to: 'questions#ask', as: :ask
+  # get 'questions/answer'
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+# get 'about', to: 'pages#about', as: :about
